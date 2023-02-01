68c886c01bf1e50044b08bc474f7b9c5063687ed
Kayleigh Watts <kwatts949@gmail.com>
2023-02-01 11:01:10 +0000

    All tests pass

--- javascript/fruit-picker/fruit-picker.js
+++ javascript/fruit-picker/fruit-picker.js
@@ -9,14 +9,14 @@
 //
 // In your own projects, files, and code, you can play with @ts-check as well.
 
-import { notify } from './notifier';
-import { order } from './grocer';
+import { notify } from "./notifier";
+import { order } from "./grocer";
 
 /**
  * @return void
  */
 export function onSuccess() {
-  // implement the onSuccess callback to call notify with a success message
+  return notify({ message: "SUCCESS" });
 }
 
 /**
@@ -24,6 +24,7 @@ export function onSuccess() {
  */
 export function onError() {
   // implement the onError callback to call notify with an error message
+  return notify({ message: "ERROR" });
 }
 
 /**
@@ -34,6 +35,8 @@ export function onError() {
  */
 export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
   // implement the orderFromGrocer function to order the query
+  // pass the params into order function
+  return order(query, onSuccessCallback, onErrorCallback);
 }
 
 /**
@@ -43,4 +46,5 @@ export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
  */
 export function postOrder(variety, quantity) {
   //implement the postOrder function to create a query and order
+  return orderFromGrocer({ variety, quantity }, onSuccess, onError);
 }
