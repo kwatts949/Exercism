0f03c1b9b6cfbde32547f41332261455f075f365
Kayleigh Watts <kwatts949@gmail.com>
2023-02-01 12:13:35 +0000

    tests pass

---
+++ javascript/elyses-looping-enchantments/enchantments.js
@@ -0,0 +1,30 @@
+// @ts-check
+
+/**
+ * Determine how many cards of a certain type there are in the deck
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} number of cards of a single type there are in the deck
+ */
+export function cardTypeCheck(stack, card) {
+  let count = 0;
+  stack.forEach((element) => {
+    if (element === card) {
+      count += 1;
+    }
+  });
+  return count;
+}
+
+/**
+ * Determine how many cards are odd or even
+ *
+ * @param {number[]} stack
+ * @param {boolean} type the type of value to check for - odd or even
+ * @returns {number} number of cards that are either odd or even (depending on `type`)
+ */
+export function determineOddEvenCards(stack, type) {
+  throw new Error("Implement the determineOddEvenCards function");
+}
