53129e396d1d2ef8b2dd6e3190b7a61032577beb
Kayleigh Watts <kwatts949@gmail.com>
2023-02-02 16:02:10 +0000

    all tests pass

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -3,10 +3,9 @@ class ResistorColorDuo
   def self.value(str)
     colours = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
   
-    p str
     number = []
     number.push(colours.find_index(str[0]))
     number.push(colours.find_index(str[1]))
-    p number.join().to_i
+    number.join().to_i
   end
 end
