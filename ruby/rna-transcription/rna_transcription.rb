6640104a8749592f6d1c681ac1b6b961265ae38b
Kayleigh Watts <kwatts949@gmail.com>
2023-02-04 20:01:10 +0000

    test passes

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,16 +1,19 @@
 class Complement
   def self.of_dna(base)
-    p base
-    if base == 'C'
-      'G'
-    elsif base == 'G'
-      'C'
-    elsif base == 'T'
-      'A'
-    elsif base == 'A'
-      'U'
-    else
-      ''
-    end
+    arr = []
+    base_array = base.split("")
+    base_array.each { |base|
+      if base == 'C'
+        arr.push('G')
+      elsif base == 'G'
+        arr.push('C')
+      elsif base == 'T'
+        arr.push('A')
+      elsif base == 'A'
+        arr.push('U')
+      else
+        ''
+    end}
+    arr.join(",").delete(",")
   end
 end
