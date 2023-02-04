7831151284a831b1900060c1376332ca9d938a4d
Kayleigh Watts <kwatts949@gmail.com>
2023-02-04 20:35:48 +0000

    test passes

--- ruby/pangram/pangram_test.rb
+++ ruby/pangram/pangram_test.rb
@@ -52,7 +52,7 @@ class PangramTest < Minitest::Test
   end
 
   def test_missing_letters_replaced_by_numbers
-    skip
+    #skip
     sentence = "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"
     result = Pangram.pangram?(sentence)
     refute result, "Expected false, got: #{result.inspect}. #{sentence.inspect} is NOT a pangram"
