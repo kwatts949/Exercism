ad3faab147385618b9c078d2cf7f5d8a6e614f50
Kayleigh Watts <kwatts949@gmail.com>
2023-02-23 09:58:54 +0000

    tests pass - expected & remaining

---
+++ java/lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,16 @@
+public class Lasagna {
+    // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+
+    // TODO: define the 'remainingMinutesInOven()' method
+    
+
+    public int remainingMinutesInOven(int minutes) {
+        return 40 - minutes;
+    }
+    // TODO: define the 'preparationTimeInMinutes()' method
+
+    // TODO: define the 'totalTimeInMinutes()' method
+}
