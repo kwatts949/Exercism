49dbc010221935096c4e4e1488ccbb911595ed9d
Kayleigh Watts <kwatts949@gmail.com>
2023-02-23 15:58:02 +0000

    tests pass

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,13 @@
 class SqueakyClean {
     static String clean(String identifier) {
-        return identifier.replace(" ", "_");
+        if(!identifier.isEmpty()) {
+        identifier = identifier
+        .replaceAll(" ", "_")
+        .replaceAll("\\p{Cntrl}", "CTRL")
+        .replaceAll("-.", String.valueOf(Character.toUpperCase(identifier.charAt(identifier.indexOf("-") + 1))))
+        .replaceAll("[α-ω]", "")
+        .replaceAll("[^\\p{L}\\p{P}\\p{Z}]", "");
+        }
+        return identifier;
     }
 }
