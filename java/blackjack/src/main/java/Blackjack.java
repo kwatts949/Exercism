def16271eb0327509203fd9adebdb0292e6d79db
Kayleigh Watts <kwatts949@gmail.com>
2023-02-23 13:35:34 +0000

    tests pass - smallHand

--- java/blackjack/src/main/java/Blackjack.java
+++ java/blackjack/src/main/java/Blackjack.java
@@ -46,7 +46,15 @@ public class Blackjack {
     }
 
     public String smallHand(int handScore, int dealerScore) {
-        throw new UnsupportedOperationException("Please implement the Blackjack.smallHand method");
+        if(handScore >= 17) {
+            return "S";
+        } else if(handScore >= 11) {
+            return "H";
+        } else if(dealerScore >= 7) {
+            return "H";
+        } else {
+            return "S";
+        }
     }
 
     // FirstTurn returns the semi-optimal decision for the first turn, given the cards of the player and the dealer.
