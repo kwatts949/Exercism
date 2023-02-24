18d737083fb5b09fde06a058fdbdcf89c45ed3da
Kayleigh Watts <kwatts949@gmail.com>
2023-02-24 15:11:57 +0000

    tests pass

---
+++ java/salary-calculator/src/main/java/SalaryCalculator.java
@@ -0,0 +1,21 @@
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+
+public class SalaryCalculator {
+    public double multiplierPerDaysSkipped(int daysSkipped) {
+        return daysSkipped < 5 ? 1 : 0.85;
+    }
+
+    public int multiplierPerProductsSold(int productsSold) {
+        return productsSold < 20 ? 10 : 13;
+    }
+
+    public double bonusForProductSold(int productsSold) {
+        return multiplierPerProductsSold(productsSold) * productsSold;
+    }
+
+    public double finalSalary(int daysSkipped, int productsSold) {
+        double salary = 1000.00 * multiplierPerDaysSkipped(daysSkipped) + bonusForProductSold(productsSold);
+
+        return salary < 2000.00 ? salary : 2000.00;
+    } 
+}
