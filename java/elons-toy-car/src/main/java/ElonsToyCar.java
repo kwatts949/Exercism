5383c5cf63a0bc1fd0a5fcd6498480aa67d87ee6
Kayleigh Watts <kwatts949@gmail.com>
2023-02-26 16:04:33 +0000

    test passes - buy

---
+++ java/elons-toy-car/src/main/java/ElonsToyCar.java
@@ -0,0 +1,18 @@
+public class ElonsToyCar {
+    public static ElonsToyCar buy() {
+        ElonsToyCar car = new ElonsToyCar();
+        return car;
+    }
+
+    public String distanceDisplay() {
+        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.distanceDisplay()  method");
+    }
+
+    public String batteryDisplay() {
+        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.batteryDisplay()  method");
+    }
+
+    public void drive() {
+        throw new UnsupportedOperationException("Please implement the (static) RemoteControlCar.drive()  method");
+    }
+}
