524c0831b975617a8807e04821ec4850ca307d44
Kayleigh Watts <kwatts949@gmail.com>
2023-02-28 10:41:44 +0000

    tests pass

--- java/football-match-reports/src/main/java/FootballMatchReports.java
+++ java/football-match-reports/src/main/java/FootballMatchReports.java
@@ -5,16 +5,20 @@ public class FootballMatchReports {
                 return "goalie";
             case 2:
                 return "left back";
-            case 3:
-                return "center back";
-            case 4:
+            case 3,4:
                 return "center back";
             case 5:
                 return "right back";
+            case 6,7,8:
+                return "midfielder";
             case 9:
                 return "left wing";
+            case 10:
+                return "striker";
+            case 11:
+                return "right wing";
             default:
-                return "midfielder";
+                throw new IllegalArgumentException();
         }
     }
 }
