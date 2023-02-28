dde48c4730c987f9c695fdc61028bb9c3f908f0e
Kayleigh Watts <kwatts949@gmail.com>
2023-02-28 10:35:26 +0000

    test passes - left & center back

--- java/football-match-reports/src/main/java/FootballMatchReports.java
+++ java/football-match-reports/src/main/java/FootballMatchReports.java
@@ -3,6 +3,12 @@ public class FootballMatchReports {
         switch(shirtNum) {
             case 1:
                 return "goalie";
+            case 2:
+                return "left back";
+            case 3:
+                return "center back";
+            case 4:
+                return "center back";
             default:
                 return "none";
         }
