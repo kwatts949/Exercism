28697b71971e7dea7afbb2bcc99069c49bfbf808
Kayleigh Watts <kwatts949@gmail.com>
2023-03-01 12:52:16 +0000

    test passes - toString

---
+++ java/wizards-and-warriors/src/main/java/Fighter.java
@@ -0,0 +1,40 @@
+abstract class Fighter {
+
+    boolean isVulnerable() {
+        throw new UnsupportedOperationException("Please provide implementation for this method");
+    }
+
+    abstract int damagePoints(Fighter fighter);
+
+}
+
+class Warrior extends Fighter {
+
+    @Override
+    public String toString() {
+        return "Fighter is a " + getClass().getName();
+    }
+
+    @Override
+    int damagePoints(Fighter wizard) {
+        throw new UnsupportedOperationException("Please implement Warrior.damagePoints() method");
+    }
+}
+
+class Wizard extends Fighter {
+
+    @Override
+    boolean isVulnerable() {
+        throw new UnsupportedOperationException("Please implement Wizard.isVulnerable() method");
+    }
+
+    @Override
+    int damagePoints(Fighter warrior) {
+        throw new UnsupportedOperationException("Please implement Wizard.damagePoints() method");
+    }
+
+    void prepareSpell() {
+        throw new UnsupportedOperationException("Please implement Wizard.prepareSpell() method");
+    }
+
+}
